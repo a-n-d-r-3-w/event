3ea36e2737870773739c54b5a30f4d9b85a56463
andr3w <liu.anray+github@gmail.com>
2018-07-07 23:12:15 -0400

    ResourceEvent, NetworkEvent, and FocusEvent

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
-};
+// Reference: https://developer.mozilla.org/en-US/docs/Web/Events
+
+exports.ResourceEvent = Object.freeze({
+  CACHED: "cached",
+  ERROR: "error",
+  ABORT: "abort",
+  LOAD: "load",
+  BEFORE_UNLOAD: "beforeunload",
+  UNLOAD: "unload"
+});
+
+exports.NetworkEvent = Object.freeze({
+  ONLINE: "online",
+  OFFLINE: "offline"
+});
+
+exports.FocusEvent = Object.freeze({
+  FOCUS: "focus",
+  BLUR: "blur"
+});
